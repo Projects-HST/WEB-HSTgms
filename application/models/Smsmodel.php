269439d77a9321cfb55446b5cf63ea9078a245e6
kamalhappysanz <kamal.happysanz@gmail.com>
2020-04-29 20:18:43 +0530

    voice call testing

--- application/models/Smsmodel.php
+++ application/models/Smsmodel.php
@@ -81,11 +81,37 @@ Class Smsmodel extends CI_Model
 			$contact_numbers = urlencode("9789108819");
 
 			$api = "http://103.255.100.37/api/voice/voice_broadcast.php?username=".$username."&token=".$token."&plan_id=".$plan_id."&announcement_id=".$announcement_id."&caller_id=".$caller_id."&contact_numbers=".$contact_numbers."";
+			//
+			// $response = file_get_contents($api);
+			//
+			// echo $response;
+			// return $response;
 
-			$response = file_get_contents($api);
+			$curl = curl_init();
+      curl_setopt_array($curl, array(
+      // CURLOPT_URL => "https://api.msg91.com/api/sendhttp.php?mobiles=$phone&authkey=301243AX0Pp4EOQCn5db82c4f&route=4&sender=SKILEX&message=$notes&country=91",
+      CURLOPT_URL => $api,
 
-			echo $response;
-			return $response;
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "GET",
+      CURLOPT_SSL_VERIFYHOST => 0,
+      CURLOPT_SSL_VERIFYPEER => 0,
+    ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+      echo "cURL Error #:" . $err;
+    } else {
+      echo $response;
+    }
 
 		}
 
