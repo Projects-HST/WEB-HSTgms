73f40ef3e4ef334664ee5eb925549b2ce31f3054
kamalhappysanz <kamal.happysanz@gmail.com>
2020-05-01 16:59:55 +0530

    profile info committed

--- application/views/admin/constituent/constituent_profile_info.php
+++ application/views/admin/constituent/constituent_profile_info.php
@@ -41,151 +41,44 @@
 
                   </div>
                   <div class="col-md-10 col-sm-9 ">
+                    <div class="profile_heading">
+                      <h4>Profile</h4>
+                    </div>
+                    <div class="col-md-12 profile_info">
+                      <div class="col-md-6">
+                          <p class="label_profile">PARTY MEMBER : <span class="label_text"><?php if($rows->party_member_status=='Y'){ echo "Yes"; }else{ echo "NO"; } ?></span></p>
+                          <p class="label_profile">vote type : <span class="label_text"><?php echo $rows->vote_type; ?></span></p>
+                          <p class="label_profile">serial no: <span class="label_text"><?php echo $rows->serial_no; ?></span></p>
+                          <p class="label_profile">status: <span class="label_text"><?php echo $rows->status; ?></span></p>
+                          <p class="label_profile">constituency: <span class="label_text"><?php echo $rows->constituency_name; ?></span></p>
+                          <p class="label_profile">paguthi: <span class="label_text"><?php echo $rows->paguthi_name; ?></span></p>
+                          <p class="label_profile">ward: <span class="label_text"><?php echo $rows->ward_name; ?></span></p>
+                          <p class="label_profile">booth: <span class="label_text"><?php echo $rows->booth_name; ?></span></p>
+                          <p class="label_profile">booth address: <span class="label_text"><?php echo $rows->booth_address; ?></span></p>
 
+
+                      </div>
+                      <div class="col-md-6">
+                        <p class="label_profile">GENDER: <span class="label_text"><?php if($rows->gender=='M'){ echo "male"; }else if($rows->gender=='M'){ echo "female";}else{ echo "others"; }  ?></span></p>
+                        <p class="label_profile">mobile no: <span class="label_text"><?php echo $rows->mobile_no; ?></span></p>
+                        <p class="label_profile">whatsapp: <span class="label_text"><?php echo $rows->whatsapp_no; ?></span></p>
+                        <p class="label_profile">email id: <span class="label_text"><?php echo $rows->email_id; ?></span></p>
+                        <p class="label_profile">religion: <span class="label_text"><?php echo $rows->religion_name; ?></span></p>
+                        <p class="label_profile">voter id: <span class="label_text"><?php echo $rows->voter_id_no; ?></span></p>
+                        <p class="label_profile">aadhaar: <span class="label_text"><?php echo $rows->aadhaar_no; ?></span></p>
+                        <p class="label_profile">father or husband: <span class="label_text"><?php echo $rows->father_husband_name; ?></span></p>
+                        <p class="label_profile">date of birth: <span class="label_text"><?php echo $rows->dob; ?></span></p>
+
+                      </div>
+                    </div>
                   <div class="" role="tabpanel" data-example-id="togglable-tabs">
                         <ul id="myTab" class="nav nav-tabs bar_tabs" role="tablist">
-                           <li role="presentation" class=""><a href="#tab_content1" id="home-tab" role="tab" data-toggle="tab" aria-expanded="true" class="" aria-selected="false">Profile</a></li>
+                           <li role="presentation" class=""><a href="#tab_content1" id="home-tab" role="tab" data-toggle="tab" aria-expanded="true" class="" aria-selected="false">Meeting and plant</a></li>
                            <li role="presentation" class=""><a href="#tab_content2" role="tab" id="profile-tab" data-toggle="tab" aria-expanded="false" class="" aria-selected="false">Grievance details</a></li>
-                           <li role="presentation" class="active"><a href="#tab_content3" role="tab" id="profile-tab2" data-toggle="tab" aria-expanded="false" class="active" aria-selected="true">Meeting and plant</a></li>
+                           <!-- <li role="presentation" class="active"><a href="#tab_content3" role="tab" id="profile-tab2" data-toggle="tab" aria-expanded="false" class="active" aria-selected="true">Meeting and plant</a></li> -->
                         </ul>
                         <div id="myTabContent" class="tab-content">
                            <div role="tabpanel" class="tab-pane fade active show" id="tab_content1" aria-labelledby="home-tab">
-                             <table class="data table table-striped no-margin">
-                                  <thead>
-
-                                  </thead>
-                                  <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td>Party member </td>
-                                        <td><?php if($rows->party_member_status=='Y'){ echo "Yes"; }else{ echo "NO"; } ?></td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>vote type </td>
-                                        <td><?php echo $rows->vote_type; ?></td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td>serial no </td>
-                                        <td><?php echo $rows->serial_no; ?></td>
-                                    </tr>
-                                    <tr>
-                                        <td>4</td>
-                                        <td>status </td>
-                                        <td><?php echo $rows->status; ?></td>
-                                    </tr>
-                                  <tr>
-                                      <td>5</td>
-                                      <td>constituency </td>
-                                      <td><?php echo $rows->constituency_name; ?></td>
-                                  </tr>
-                                  <tr>
-                                      <td>6</td>
-                                      <td>paguthi </td>
-                                      <td><?php echo $rows->paguthi_name; ?></td>
-                                  </tr>
-                                  <tr>
-                                      <td>7</td>
-                                      <td>ward </td>
-                                      <td><?php echo $rows->ward_name; ?></td>
-                                  </tr>
-                                  <tr>
-                                      <td>8</td>
-                                      <td>booth </td>
-                                      <td><?php echo $rows->booth_name; ?></td>
-                                  </tr>
-                                  <tr>
-                                      <td>9</td>
-                                      <td>booth address </td>
-                                      <td><?php echo $rows->booth_address; ?></td>
-                                  </tr>
-                                  <tr>
-                                      <td>10</td>
-                                      <td>gender </td>
-                                      <td><?php if($rows->gender=='M'){ echo "male"; }else{ echo "female"; }  ?></td>
-                                  </tr>
-                                  <tr>
-                                      <td>11</td>
-                                      <td>mobile no </td>
-                                      <td><?php echo $rows->mobile_no; ?></td>
-                                  </tr>
-                                  <tr>
-                                      <td>12</td>
-                                      <td>whatsapp no </td>
-                                      <td><?php echo $rows->whatsapp_no; ?></td>
-                                  </tr>
-                                  <tr>
-                                      <td>13</td>
-                                      <td>religion name </td>
-                                      <td><?php echo $rows->religion_name; ?></td>
-                                  </tr>
-                                  <tr>
-                                      <td>14</td>
-                                      <td>voter id </td>
-                                      <td><?php echo $rows->voter_id_no; ?></td>
-                                  </tr>
-                                  <tr>
-                                      <td>15</td>
-                                      <td>aadhaar no </td>
-                                      <td><?php echo $rows->aadhaar_no; ?></td>
-                                  </tr>
-                                  <tr>
-                                      <td>16</td>
-                                      <td>email  </td>
-                                      <td><?php echo $rows->email_id; ?></td>
-                                  </tr>
-                                  <tr>
-                                      <td>17</td>
-                                      <td>father or husband name  </td>
-                                      <td><?php echo $rows->father_husband_name; ?></td>
-                                  </tr>
-                                  <tr>
-                                      <td>18</td>
-                                      <td>date of birth  </td>
-                                      <td><?php echo $rows->dob; ?></td>
-                                  </tr>
-
-
-                                  </tbody>
-                                  </table>
-                           </div>
-                           <div role="tabpanel" class="tab-pane fade" id="tab_content2" aria-labelledby="profile-tab">
-                             <table id="example_4" class="table table-striped table-bordered dt-responsive nowrap" style="width:100%">
-                                <thead>
-                                   <tr>
-                                      <th>S.no</th>
-                                      <th>seeker</th>
-                                      <!-- <th>category</th> -->
-                                      <th>sub category</th>
-                                      <th>petition no</th>
-                                      <th>reference</th>
-                                      <th>status</th>
-                                      <th>updated at</th>
-
-                                   </tr>
-                                </thead>
-                                <tbody>
-                                  <?php $i=1; foreach($res_grievance as $rows_grievance){ ?>
-                                    <tr>
-                                      <td><?php echo $i; ?></td>
-
-
-                                      <td><?php echo $rows_grievance->seeker_info; ?></td>
-                                      <!-- <td><?php echo $rows_grievance->grievance_name; ?></td> -->
-                                      <td><?php echo $rows_grievance->sub_category_name; ?></td>
-                                      <td><?php echo $rows_grievance->petition_enquiry_no; ?></td>
-                                      <td><?php echo $status= $rows_grievance->reference_note; ?></td>
-                                      <td><?php echo $status= $rows_grievance->status; ?></td>
-                                      <td><?php echo $rows_grievance->updated_at; ?></td>
-
-                                      </tr>
-                                <?php $i++; } ?>
-
-                                </tbody>
-                             </table>
-                           </div>
-                           <div role="tabpanel" class="tab-pane fade active" id="tab_content3" aria-labelledby="meeting-tab">
                              <h4>Plant donation</h4>
                              <table id="example_3" class="table table-striped table-bordered dt-responsive nowrap" style="width:100%">
                                 <thead>
@@ -236,6 +129,42 @@
                              </table>
 
                            </div>
+                           <div role="tabpanel" class="tab-pane fade" id="tab_content2" aria-labelledby="profile-tab">
+                             <table id="example_4" class="table table-striped table-bordered dt-responsive nowrap" style="width:100%">
+                                <thead>
+                                   <tr>
+                                      <th>S.no</th>
+                                      <th>seeker</th>
+                                      <!-- <th>category</th> -->
+                                      <th>sub category</th>
+                                      <th>petition no</th>
+                                      <th>reference</th>
+                                      <th>status</th>
+                                      <th>updated at</th>
+
+                                   </tr>
+                                </thead>
+                                <tbody>
+                                  <?php $i=1; foreach($res_grievance as $rows_grievance){ ?>
+                                    <tr>
+                                      <td><?php echo $i; ?></td>
+
+
+                                      <td><?php echo $rows_grievance->seeker_info; ?></td>
+                                      <!-- <td><?php echo $rows_grievance->grievance_name; ?></td> -->
+                                      <td><?php echo $rows_grievance->sub_category_name; ?></td>
+                                      <td><?php echo $rows_grievance->petition_enquiry_no; ?></td>
+                                      <td><?php echo $status= $rows_grievance->reference_note; ?></td>
+                                      <td><?php echo $status= $rows_grievance->status; ?></td>
+                                      <td><?php echo $rows_grievance->updated_at; ?></td>
+
+                                      </tr>
+                                <?php $i++; } ?>
+
+                                </tbody>
+                             </table>
+                           </div>
+
                         </div>
                      </div>
                   </div>
