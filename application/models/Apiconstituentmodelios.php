23079896b2527681e942c5f449c855286d4ab9a4
infeg-khaleela <66429354+infeg-khaleela@users.noreply.github.com>
2020-07-07 12:33:10 +0530

    Committed

--- application/models/Apiconstituentmodelios.php
+++ application/models/Apiconstituentmodelios.php
@@ -97,7 +97,7 @@ function version_check($version_code){
             'full_name'=>$rows->full_name,
             'father_husband_name'=>$rows->father_husband_name,
             'serial_no'=>$rows->serial_no,
-            'dob'=>$dob,
+            'dob'=>$dob,            
             'profile_picture'=>$pic,
           );
         }
