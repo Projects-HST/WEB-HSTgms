d5d4310d7e23b9a25f7d63d187f84b02f8113885
Maran <maran.happysanz@gmail.com>
2020-07-09 17:40:17 +0530

    Update Apiios.php

--- application/controllers/Apiios.php
+++ application/controllers/Apiios.php
@@ -42,7 +42,7 @@ class Apiios extends CI_Controller {
 
 		$version_code = '';
 		$version_code = $this->input->post("version_code");
-		$data['result']=$this->apiandroidmodel->version_check($version_code);
+		$data['result']=$this->apiiosmodel->version_check($version_code);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -70,7 +70,7 @@ class Apiios extends CI_Controller {
 		$gcmkey = $this->input->post("device_id");
 		$mobiletype = $this->input->post("mobile_type");
 
-		$data['result']=$this->apiandroidmodel->Login(strtoupper($username),strtoupper($password),$gcmkey,$mobiletype);
+		$data['result']=$this->apiiosmodel->Login(strtoupper($username),strtoupper($password),$gcmkey,$mobiletype);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -89,7 +89,7 @@ class Apiios extends CI_Controller {
 		$user_name = '';
 		$user_name = $this->input->post("user_name");
 		
-		$data['result']=$this->apiandroidmodel->Forgot_password(strtoupper($user_name));
+		$data['result']=$this->apiiosmodel->Forgot_password(strtoupper($user_name));
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -109,7 +109,7 @@ class Apiios extends CI_Controller {
         $user_id = '';
         $user_id = $this->input->post("user_id");
 
-		$data['result']=$this->apiandroidmodel->Profile_details($user_id);
+		$data['result']=$this->apiiosmodel->Profile_details($user_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -131,7 +131,7 @@ class Apiios extends CI_Controller {
         $email = '';
         $email = $this->input->post("email");
 
-		$data['result']=$this->apiandroidmodel->Check_email(strtoupper($email));
+		$data['result']=$this->apiiosmodel->Check_email(strtoupper($email));
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -155,7 +155,7 @@ class Apiios extends CI_Controller {
 		$user_id = $this->input->post("user_id");
         $email = $this->input->post("email");
 
-		$data['result']=$this->apiandroidmodel->Check_emailedit($user_id,strtoupper($email));
+		$data['result']=$this->apiiosmodel->Check_emailedit($user_id,strtoupper($email));
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -176,7 +176,7 @@ class Apiios extends CI_Controller {
         $phone = '';
         $phone = $this->input->post("phone");
 
-		$data['result']=$this->apiandroidmodel->Check_phone($phone);
+		$data['result']=$this->apiiosmodel->Check_phone($phone);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -200,7 +200,7 @@ class Apiios extends CI_Controller {
 		$user_id = $this->input->post("user_id");
         $phone = $this->input->post("phone");
 
-		$data['result']=$this->apiandroidmodel->Check_phoneedit($user_id,$phone);
+		$data['result']=$this->apiiosmodel->Check_phoneedit($user_id,$phone);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -232,7 +232,7 @@ class Apiios extends CI_Controller {
 		$email=$this->input->post('email');
 		$gender=$this->input->post('gender');
 
-		$data['result']=$this->apiandroidmodel->Profile_update(strtoupper($name),strtoupper($address),$phone,strtoupper($email),$gender,$user_id);
+		$data['result']=$this->apiiosmodel->Profile_update(strtoupper($name),strtoupper($address),$phone,strtoupper($email),$gender,$user_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -252,7 +252,7 @@ class Apiios extends CI_Controller {
 		$profilepic = $uploaddir.$staff_prof_pic;
 		move_uploaded_file($_FILES['user_pic']['tmp_name'], $profilepic);
 
-		$data['result']=$this->apiandroidmodel->Update_profilepic($user_id,$staff_prof_pic);
+		$data['result']=$this->apiiosmodel->Update_profilepic($user_id,$staff_prof_pic);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -278,7 +278,7 @@ class Apiios extends CI_Controller {
 	 	$newpassword = $this->input->post("new_password");
 		$oldpassword = $this->input->post("old_password");
 
-		$data['result']=$this->apiandroidmodel->Change_password($user_id,$newpassword,$oldpassword);
+		$data['result']=$this->apiiosmodel->Change_password($user_id,$newpassword,$oldpassword);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -299,7 +299,7 @@ class Apiios extends CI_Controller {
 		$constituency_id = '';
 		$constituency_id = $this->input->post("constituency_id");
 
-		$data['result']=$this->apiandroidmodel->List_paguthi($constituency_id);
+		$data['result']=$this->apiiosmodel->List_paguthi($constituency_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -320,7 +320,7 @@ class Apiios extends CI_Controller {
 		$paguthi = '';
 		$paguthi = $this->input->post("paguthi");
 
-		$data['result']=$this->apiandroidmodel->Dashboard($paguthi);
+		$data['result']=$this->apiiosmodel->Dashboard($paguthi);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -341,7 +341,7 @@ class Apiios extends CI_Controller {
 		$paguthi = '';
 		$paguthi = $this->input->post("paguthi");
 
-		$data['result']=$this->apiandroidmodel->Widgets_members($paguthi);
+		$data['result']=$this->apiiosmodel->Widgets_members($paguthi);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -362,7 +362,7 @@ class Apiios extends CI_Controller {
 		$paguthi = '';
 		$paguthi = $this->input->post("paguthi");
 
-		$data['result']=$this->apiandroidmodel->Widgets_meetings($paguthi);
+		$data['result']=$this->apiiosmodel->Widgets_meetings($paguthi);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -383,7 +383,7 @@ class Apiios extends CI_Controller {
 		$paguthi = '';
 		$paguthi = $this->input->post("paguthi");
 
-		$data['result']=$this->apiandroidmodel->Widgets_grievances($paguthi);
+		$data['result']=$this->apiiosmodel->Widgets_grievances($paguthi);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -404,7 +404,7 @@ class Apiios extends CI_Controller {
 		$paguthi = '';
 		$paguthi = $this->input->post("paguthi");
 
-		$data['result']=$this->apiandroidmodel->Widgets_interactions($paguthi);
+		$data['result']=$this->apiiosmodel->Widgets_interactions($paguthi);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -425,7 +425,7 @@ class Apiios extends CI_Controller {
 		$keyword = '';
 		$keyword = $this->input->post("keyword");
 
-		$data['result']=$this->apiandroidmodel->Dashboard_search($keyword);
+		$data['result']=$this->apiiosmodel->Dashboard_search($keyword);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -449,7 +449,7 @@ class Apiios extends CI_Controller {
 		$keyword = $this->input->post("keyword");
 		$offset = $this->input->post("offset");
 		$rowcount = $this->input->post("rowcount");
-		$data['result']=$this->apiandroidmodel->Dashboard_searchnew($keyword,$offset,$rowcount);
+		$data['result']=$this->apiiosmodel->Dashboard_searchnew($keyword,$offset,$rowcount);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -469,7 +469,7 @@ class Apiios extends CI_Controller {
 
 		$paguthi = '';
 		$paguthi = $this->input->post("paguthi");
-		$data['result']=$this->apiandroidmodel->List_constituent($paguthi);
+		$data['result']=$this->apiiosmodel->List_constituent($paguthi);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -493,7 +493,7 @@ class Apiios extends CI_Controller {
 		$paguthi = $this->input->post("paguthi");
 		$offset = $this->input->post("offset");
 		$rowcount = $this->input->post("rowcount");
-		$data['result']=$this->apiandroidmodel->List_constituentnew($paguthi,$offset,$rowcount);
+		$data['result']=$this->apiiosmodel->List_constituentnew($paguthi,$offset,$rowcount);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -519,7 +519,7 @@ class Apiios extends CI_Controller {
 		$paguthi = $this->input->post("paguthi");
 		$offset = $this->input->post("offset");
 		$rowcount = $this->input->post("rowcount");
-		$data['result']=$this->apiandroidmodel->List_constituentsearch($keyword,$paguthi,$offset,$rowcount);
+		$data['result']=$this->apiiosmodel->List_constituentsearch($keyword,$paguthi,$offset,$rowcount);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -539,7 +539,7 @@ class Apiios extends CI_Controller {
 
 		$constituent_id = '';
 		$constituent_id = $this->input->post("constituent_id");
-		$data['result']=$this->apiandroidmodel->Constituent_details($constituent_id);
+		$data['result']=$this->apiiosmodel->Constituent_details($constituent_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -559,7 +559,7 @@ class Apiios extends CI_Controller {
 
 		$constituent_id = '';
 		$constituent_id = $this->input->post("constituent_id");
-		$data['result']=$this->apiandroidmodel->Constituent_meetings($constituent_id);
+		$data['result']=$this->apiiosmodel->Constituent_meetings($constituent_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -579,7 +579,7 @@ class Apiios extends CI_Controller {
 
 		$meeting_id = '';
 		$meeting_id = $this->input->post("meeting_id");
-		$data['result']=$this->apiandroidmodel->Constituent_meetingdetails($meeting_id);
+		$data['result']=$this->apiiosmodel->Constituent_meetingdetails($meeting_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -599,7 +599,7 @@ class Apiios extends CI_Controller {
 
 		$constituent_id = '';
 		$constituent_id = $this->input->post("constituent_id");
-		$data['result']=$this->apiandroidmodel->Constituent_grievances($constituent_id);
+		$data['result']=$this->apiiosmodel->Constituent_grievances($constituent_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -619,7 +619,7 @@ class Apiios extends CI_Controller {
 
 		$grievance_id = '';
 		$grievance_id = $this->input->post("grievance_id");
-		$data['result']=$this->apiandroidmodel->Constituent_grievancedetails($grievance_id);
+		$data['result']=$this->apiiosmodel->Constituent_grievancedetails($grievance_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -639,7 +639,7 @@ class Apiios extends CI_Controller {
 
 		$grievance_id = '';
 		$grievance_id = $this->input->post("grievance_id");
-		$data['result']=$this->apiandroidmodel->Grievance_message($grievance_id);
+		$data['result']=$this->apiiosmodel->Grievance_message($grievance_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -660,7 +660,7 @@ class Apiios extends CI_Controller {
 
 		$constituent_id = '';
 		$constituent_id = $this->input->post("constituent_id");
-		$data['result']=$this->apiandroidmodel->Constituent_interaction($constituent_id);
+		$data['result']=$this->apiiosmodel->Constituent_interaction($constituent_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -680,7 +680,7 @@ class Apiios extends CI_Controller {
 
 		$constituent_id = '';
 		$constituent_id = $this->input->post("constituent_id");
-		$data['result']=$this->apiandroidmodel->Constituent_plant($constituent_id);
+		$data['result']=$this->apiiosmodel->Constituent_plant($constituent_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -700,7 +700,7 @@ class Apiios extends CI_Controller {
 
 		$constituent_id = '';
 		$constituent_id = $this->input->post("constituent_id");
-		$data['result']=$this->apiandroidmodel->Constituent_documents($constituent_id);
+		$data['result']=$this->apiiosmodel->Constituent_documents($constituent_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -721,7 +721,7 @@ class Apiios extends CI_Controller {
 
 		$constituent_id = '';
 		$constituent_id = $this->input->post("constituent_id");
-		$data['result']=$this->apiandroidmodel->Constituent_grvdocuments($constituent_id);
+		$data['result']=$this->apiiosmodel->Constituent_grvdocuments($constituent_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -742,7 +742,7 @@ class Apiios extends CI_Controller {
 		$constituency_id = '';
 		$constituency_id = $this->input->post("constituency_id");
 		
-		$data['result']=$this->apiandroidmodel->Meeting_request($constituency_id);
+		$data['result']=$this->apiiosmodel->Meeting_request($constituency_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -768,7 +768,7 @@ class Apiios extends CI_Controller {
 		$offset = $this->input->post("offset");
 		$rowcount = $this->input->post("rowcount");
 		
-		$data['result']=$this->apiandroidmodel->Meeting_requestnew($constituency_id,$offset,$rowcount);
+		$data['result']=$this->apiiosmodel->Meeting_requestnew($constituency_id,$offset,$rowcount);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -797,7 +797,7 @@ class Apiios extends CI_Controller {
 		$offset = $this->input->post("offset");
 		$rowcount = $this->input->post("rowcount");
 		
-		$data['result']=$this->apiandroidmodel->Meeting_requestsearch($constituency_id,$keyword,$offset,$rowcount);
+		$data['result']=$this->apiiosmodel->Meeting_requestsearch($constituency_id,$keyword,$offset,$rowcount);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -817,7 +817,7 @@ class Apiios extends CI_Controller {
 
 		$meeting_id = '';
 		$meeting_id = $this->input->post("meeting_id");
-		$data['result']=$this->apiandroidmodel->Meeting_details($meeting_id);
+		$data['result']=$this->apiiosmodel->Meeting_details($meeting_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -843,7 +843,7 @@ class Apiios extends CI_Controller {
 		$meeting_id = $this->input->post("meeting_id");
 		$status = $this->input->post("status");
 		
-		$data['result']=$this->apiandroidmodel->Meeting_update($user_id,$meeting_id,strtoupper($status));
+		$data['result']=$this->apiiosmodel->Meeting_update($user_id,$meeting_id,strtoupper($status));
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -866,7 +866,7 @@ class Apiios extends CI_Controller {
 		$paguthi = $this->input->post("paguthi");
 		$grievance_type = $this->input->post("grievance_type");
 
-		$data['result']=$this->apiandroidmodel->List_grievance($paguthi,$grievance_type);
+		$data['result']=$this->apiiosmodel->List_grievance($paguthi,$grievance_type);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -894,7 +894,7 @@ class Apiios extends CI_Controller {
 		$offset = $this->input->post("offset");
 		$rowcount = $this->input->post("rowcount");
 
-		$data['result']=$this->apiandroidmodel->List_grievancenew($paguthi,$grievance_type,$offset,$rowcount);
+		$data['result']=$this->apiiosmodel->List_grievancenew($paguthi,$grievance_type,$offset,$rowcount);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -924,7 +924,7 @@ class Apiios extends CI_Controller {
 		$offset = $this->input->post("offset");
 		$rowcount = $this->input->post("rowcount");
 
-		$data['result']=$this->apiandroidmodel->List_grievancesearch($keyword,$paguthi,$grievance_type,$offset,$rowcount);
+		$data['result']=$this->apiiosmodel->List_grievancesearch($keyword,$paguthi,$grievance_type,$offset,$rowcount);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -945,7 +945,7 @@ class Apiios extends CI_Controller {
 		$constituency_id = '';
 		$constituency_id = $this->input->post("constituency_id");
 
-		$data['result']=$this->apiandroidmodel->List_staff($constituency_id);
+		$data['result']=$this->apiiosmodel->List_staff($constituency_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -966,7 +966,7 @@ class Apiios extends CI_Controller {
 		$staff_id = '';
 		$staff_id = $this->input->post("staff_id");
 
-		$data['result']=$this->apiandroidmodel->Staff_details($staff_id);
+		$data['result']=$this->apiiosmodel->Staff_details($staff_id);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -994,7 +994,7 @@ class Apiios extends CI_Controller {
 		 $status=$this->input->post('status');
 		 $paguthi=$this->input->post('paguthi');
 		
-		$data['result']=$this->apiandroidmodel->Report_status($from_date,$to_date,$status,$paguthi);
+		$data['result']=$this->apiiosmodel->Report_status($from_date,$to_date,$status,$paguthi);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -1020,7 +1020,7 @@ class Apiios extends CI_Controller {
 		$to_date = $this->input->post("to_date");	
 		$category=$this->input->post('category');
 		
-		$data['result']=$this->apiandroidmodel->Report_category($from_date,$to_date,$category);
+		$data['result']=$this->apiiosmodel->Report_category($from_date,$to_date,$category);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -1046,7 +1046,7 @@ class Apiios extends CI_Controller {
 		$to_date = $this->input->post("to_date");	
 		$sub_category=$this->input->post('sub_category');
 		
-		$data['result']=$this->apiandroidmodel->Report_subcategory($from_date,$to_date,$sub_category);
+		$data['result']=$this->apiiosmodel->Report_subcategory($from_date,$to_date,$sub_category);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -1072,7 +1072,7 @@ class Apiios extends CI_Controller {
 		$to_date = $this->input->post("to_date");	
 		$paguthi=$this->input->post('paguthi');
 		
-		$data['result']=$this->apiandroidmodel->Report_location($from_date,$to_date,$paguthi);
+		$data['result']=$this->apiiosmodel->Report_location($from_date,$to_date,$paguthi);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -1096,7 +1096,7 @@ class Apiios extends CI_Controller {
 		$from_date = $this->input->post("from_date");
 		$to_date = $this->input->post("to_date");	
 		
-		$data['result']=$this->apiandroidmodel->Report_meetings($from_date,$to_date);
+		$data['result']=$this->apiiosmodel->Report_meetings($from_date,$to_date);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -1121,7 +1121,7 @@ class Apiios extends CI_Controller {
 		$from_date = $this->input->post("from_date");
 		$to_date = $this->input->post("to_date");	
 		
-		$data['result']=$this->apiandroidmodel->Report_staff($from_date,$to_date);
+		$data['result']=$this->apiiosmodel->Report_staff($from_date,$to_date);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
@@ -1142,7 +1142,7 @@ class Apiios extends CI_Controller {
 		$selMonth = '';
 		$selMonth = $this->input->post("select_month");
 		
-		$data['result']=$this->apiandroidmodel->Report_birthday($selMonth);
+		$data['result']=$this->apiiosmodel->Report_birthday($selMonth);
 		$response = $data['result'];
 		echo json_encode($response);
 	}
