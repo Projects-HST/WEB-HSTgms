fb77928abc298d5f312ba5a1831534a330f61ed5
Maran <maran.happysanz@gmail.com>
2020-07-09 15:55:46 +0530

    Update Apiandroid.php

--- application/controllers/Apiandroid.php
+++ application/controllers/Apiandroid.php
@@ -779,7 +779,7 @@ class Apiandroid extends CI_Controller {
 
 	public function meetingRequestsearch()
 	{
-		//$_POST = json_decode(file_get_contents("php://input"), TRUE);
+		$_POST = json_decode(file_get_contents("php://input"), TRUE);
 		
 		if(!$this->checkMethod())
 		{
