4ee97bd6269af96c474b0f0a5803270c7262c322
Maran <maran.happysanz@gmail.com>
2020-07-09 17:39:06 +0530

    ios changes
    ios changes

--- application/controllers/Apiios.php
+++ application/controllers/Apiios.php
@@ -10,7 +10,7 @@ class Apiios extends CI_Controller {
 	function __construct()
     {
         parent::__construct();
-		$this->load->model("apiiosdmodel");
+		$this->load->model("apiiosmodel");
 		$this->load->helper("url");
 		$this->load->library('session');
     }
--- application/models/Apiiosmodel.php
+++ application/models/Apiiosmodel.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Apiiosdmodel extends CI_Model {
+class Apiiosmodel extends CI_Model {
 
 public function __construct()
 	{
