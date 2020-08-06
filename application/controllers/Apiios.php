6b61f88ad312b00fd26b25b566bfe9dac4ffe230
Maran <maran.happysanz@gmail.com>
2020-08-06 13:10:10 +0530

    Update Apiios.php

--- application/controllers/Apiios.php
+++ application/controllers/Apiios.php
@@ -313,7 +313,7 @@ class Apiios extends CI_Controller {
 
 	public function dashBoard()
 	{
-		//$_POST = json_decode(file_get_contents("php://input"), TRUE);
+		$_POST = json_decode(file_get_contents("php://input"), TRUE);
 		
 		if(!$this->checkMethod())
 		{
