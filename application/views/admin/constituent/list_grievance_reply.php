2f7a70a033ea75ec31d6393a93507639e0c5e63a
kamal <>
2020-08-19 11:33:22 +0530

    reply committed

--- application/views/admin/constituent/list_grievance_reply.php
+++ application/views/admin/constituent/list_grievance_reply.php
@@ -3,54 +3,14 @@
 th{
   width:100px;
 }
-.pagination-first-tag{
-	border:1px solid #eeeeee;
-	padding:10px;
-	background:#31aa15;
-}
 
-.pagination-last-tag{
-	border:1px solid #eeeeee;
-	padding:10px;
-	background:#31aa15;
-
-}
-.pagination-next-tag{
-	padding:10px;
-	border:1px solid #eeeeee;
-	background:#31aa15;
-}
-
-.pagination-prev-tag{
-	padding:10px;
-	border:1px solid #eeeeee;
-	background:#31aa15;
-
-}
-
-.pagination-current-tag{
-	color:#000000;
-	font-weight:bold;
-	padding:10px;
-	border:1px solid #eeeeee;
-}
-
-.pagination-number{
-	padding:10px;
-	border:1px solid #eeeeee;
-}
-
-.pagination-first-tag a, .pagination-next-tag a, .pagination-last-tag a, .pagination-prev-tag a{
-	color:#ffffff;
-
-}
 	</style>
   <div  class="right_col" role="main">
    <div class="">
 
      <div class="x_panel">
        <div class="">
-          <h2><i class="fa fa-file-word-o"></i> list of grievance reply</h2>
+          <h2>list of grievance reply</h2>
 
        </div>
        <?php if($this->session->flashdata('msg')) {
