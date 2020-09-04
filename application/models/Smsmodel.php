e85a61181faaa10d03b5e7de9335e4f320e0e7db
kamal <>
2020-09-04 17:14:01 +0530

    Committed

--- application/models/Smsmodel.php
+++ application/models/Smsmodel.php
@@ -11,21 +11,20 @@ Class Smsmodel extends CI_Model
 
 	public function sendSMS($to_phone,$smsContent)
 	{
+			$newstr = str_replace ( "'&","", $smsContent );
+			$msg=urlencode($newstr);
 
-			$msg=urlencode($smsContent);
+
 			 $curl = curl_init();
 			 $url="https://api.msg91.com/api/sendhttp.php?authkey=308533AMShxOBgKSt75df73187&mobiles=$to_phone&country=91&message=$smsContent&sender=GMSADM&route=4&unicode=1";
-			   // $url="http://api.msg91.com/api/v2/sendsms?authkey=308533AMShxOBgKSt75df73187&mobiles=$to_phone&message=$uni_code&sender=GMSADM&route=4&country=91";
-
-
-		curl_setopt_array($curl, array(
+			curl_setopt_array($curl, array(
 		  CURLOPT_URL => $url,
 		  CURLOPT_RETURNTRANSFER => true,
 		  CURLOPT_ENCODING => "",
 		  CURLOPT_MAXREDIRS => 10,
 		  CURLOPT_TIMEOUT => 30,
 		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => "GET",
+		  CURLOPT_CUSTOMREQUEST => "POST",
 		  CURLOPT_SSL_VERIFYHOST => 0,
 		  CURLOPT_SSL_VERIFYPEER => 0,
 		  CURLOPT_HTTPHEADER => array(
