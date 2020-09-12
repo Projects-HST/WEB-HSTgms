fc905254c4be464b62acf5d2f3b326e8ec6f8b39
kamal <>
2020-09-12 10:29:31 +0530

    Header title committed

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -9,7 +9,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="icon" href="images/favicon.ico" type="image/ico" />
-      <title>GRIEVANCE MANAGEMENT SYSTEM </title>
+      <title>GMS </title>
       <link href="<?php echo base_url(); ?>assets/admin/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
       <link href="<?php echo base_url(); ?>assets/admin/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
       <link href="<?php echo base_url(); ?>assets/admin/vendors/nprogress/nprogress.css" rel="stylesheet">
