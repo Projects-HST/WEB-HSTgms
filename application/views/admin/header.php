db81c15f371961c34e3114f5bbf5060096ceab40
kamal <>
2020-09-12 10:20:06 +0530

    Comment line removed

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -4,16 +4,12 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
    <head>
 		   <meta charset="utf-8">
-
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="icon" href="images/favicon.ico" type="image/ico" />
       <title>GRIEVANCE MANAGEMENT SYSTEM </title>
-			<!-- <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet"> -->
-			<!-- <link href="https://fonts.googleapis.com/css2?family=Heebo&display=swap" rel="stylesheet"> -->
       <link href="<?php echo base_url(); ?>assets/admin/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
       <link href="<?php echo base_url(); ?>assets/admin/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
       <link href="<?php echo base_url(); ?>assets/admin/vendors/nprogress/nprogress.css" rel="stylesheet">
