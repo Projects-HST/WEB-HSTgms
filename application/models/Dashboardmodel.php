f1be421c3f910cb4e9451d94b5316f8156c7945f
kamal <>
2020-09-16 15:32:59 +0530

    Dashboard committed

--- application/models/Dashboardmodel.php
+++ application/models/Dashboardmodel.php
@@ -44,8 +44,9 @@ Class Dashboardmodel extends CI_Model
 
 			$dateTime2 = new DateTime($to_date);
 			$two_date=date_format($dateTime2,'Y-m-d' );
+					// echo $paguthi_id;exit;
+			if($paguthi_id==' ' || $office_id==' '){
 
-			if(empty($quer_paguthi)){
 				$quer_date="WHERE DATE(created_at) BETWEEN '$one_date' and '$two_date'";
 			}else{
 				$quer_date="AND DATE(created_at) BETWEEN '$one_date' and '$two_date'";
