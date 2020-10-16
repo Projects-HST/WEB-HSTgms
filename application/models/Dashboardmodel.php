840e649087d09cb6471579a2bf0808412901ae40
kamal <>
2020-10-16 16:24:36 +0530

    date desc

--- application/models/Dashboardmodel.php
+++ application/models/Dashboardmodel.php
@@ -421,7 +421,7 @@ Class Dashboardmodel extends CI_Model
 		IFNULL(sum(case when g.repeated_status = 'R' then 1 else 0 end),'0') AS repeat_count,
 		IFNULL(count(*),'0') as total
 		FROM grievance as g
-		left join constituent as c on c.id=g.constituent_id $quer_paguthi $quer_office $quer_date order by day_name desc limit 30";
+		left join constituent as c on c.id=g.constituent_id $quer_paguthi $quer_office $quer_date order by g.grievance_date desc limit 30";
 
 
 
