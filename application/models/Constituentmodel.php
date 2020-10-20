434b864e7b321ccf3d7d31e09f836938126ffd00
kamal <>
2020-10-20 09:42:55 +0530

    committed

--- application/models/Constituentmodel.php
+++ application/models/Constituentmodel.php
@@ -896,7 +896,7 @@ function getConstituent($rowno,$rowperpage,$search_text="") {
 		if ($search_key_2 != ''){
 			 $this->db->or_like('full_name', $search_key_1,'after');
 			 $this->db->or_like('father_husband_name', $search_key_1,'after');
-			 $this->db->or_like('address', $search_key_2);
+			 $this->db->where('address', $search_key_2);
 		} else {
 			$this->db->or_like('full_name', $search_key_1,'after');
 			$this->db->or_like('father_husband_name', $search_key_1,'after');
@@ -940,8 +940,9 @@ function getConstituent($rowno,$rowperpage,$search_text="") {
 							}
 
 				if ($search_key_2 != ''){
-					 $this->db->like('full_name', $search_key_1,'after');
-					 $this->db->like('address', $search_key_2);
+					$this->db->or_like('full_name', $search_key_1,'after');
+					$this->db->or_like('father_husband_name', $search_key_1,'after');
+					$this->db->where('address', $search_key_2);
 				} else {
 					$this->db->or_like('full_name', $search_key_1,'after');
 					$this->db->or_like('mobile_no', $search_key_1);
