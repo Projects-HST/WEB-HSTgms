d3542e76847394468846da9e7d47fc3a9cf7cee8
kamal <>
2020-10-20 09:44:11 +0530

    committed

--- application/models/Constituentmodel.php
+++ application/models/Constituentmodel.php
@@ -940,8 +940,8 @@ function getConstituent($rowno,$rowperpage,$search_text="") {
 							}
 
 				if ($search_key_2 != ''){
-					$this->db->or_like('full_name', $search_key_1,'after');
-					$this->db->or_like('father_husband_name', $search_key_1,'after');
+					$this->db->where('full_name', $search_key_1,'after');
+					$this->db->where('father_husband_name', $search_key_1,'after');
 					$this->db->where('address', $search_key_2);
 				} else {
 					$this->db->or_like('full_name', $search_key_1,'after');
